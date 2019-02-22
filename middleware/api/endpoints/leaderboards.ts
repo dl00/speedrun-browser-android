8fa918029383c9867bcb8d246e9cacac40f69b41
Daniel Beal <git@danb.email>
2019-02-22 00:55:38 -0800

    fix leaderboard should still include embedded player info
    otherwise the app crashes

--- middleware/api/endpoints/leaderboards.ts
+++ middleware/api/endpoints/leaderboards.ts
@@ -4,6 +4,7 @@ import * as _ from 'lodash';
 
 import { Router } from 'express';
 
+import * as speedrun_api from '../../lib/speedrun-api'
 import * as speedrun_db from '../../lib/speedrun-db';
 
 import * as api from '../';
@@ -21,10 +22,35 @@ router.get('/:ids', async (req, res) => {
 
     let leaderboards_raw = await api.storedb!.hmget(speedrun_db.locs.leaderboards, ...ids);
 
-    let leaderboards = _.chain(leaderboards_raw)
+    let leaderboards: speedrun_api.Leaderboard[] = <any[]>_.chain(leaderboards_raw)
         .reject(_.isNil)
         .map(JSON.parse)
         .value();
+    
+    for(let leaderboard of leaderboards) {
+        if(!leaderboard.players) {
+
+            let player_ids = _.chain(leaderboard.runs)
+                .map(v => _.map(v.run.players, 'id'))
+                .flatten()
+                .reject(_.isNil)
+                .uniq()
+                .value();
+            
+            if(!player_ids.length) {
+                leaderboard.players = {};
+                continue;
+            }
+
+            let players_raw = await api.storedb!.hmget(speedrun_db.locs.players, ...player_ids);
+
+            leaderboard.players = <any>_.chain(players_raw)
+                .reject(_.isNil)
+                .map(JSON.parse)
+                .keyBy('id')
+                .value();
+        }
+    }
 
     return api_response.complete(res, leaderboards);
 });
