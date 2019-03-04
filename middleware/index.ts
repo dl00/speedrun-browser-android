bf14bda0367be961b8c83dffc695e0ab3dd93cb1
Daniel Beal <beald@buyme360.com>
2019-03-04 16:53:49 +0000

    fix variable error

--- middleware/index.ts
+++ middleware/index.ts
@@ -1,21 +1,21 @@
-import { config } from './lib/config';
+import { load_config } from './lib/config';
 
 import * as scraper from './scraper';
 import * as api from './api';
 
-export function start_scraper() {
+export function start_scraper(config: any) {
     scraper.run(config);
 }
 
-export function start_server() {
+export function start_server(config: any) {
     api.run(config);
 }
 
 export function run() {
-    config.load_config();
+    let config = load_config();
     
-    start_scraper();
-    start_server();
+    start_scraper(config);
+    start_server(config);
 }
 
 if(require.main == module) {
