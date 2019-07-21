f4f846b9bf16a5cc7076357a6eff68a8100a800d
Daniel Beal <git@danb.email>
2019-07-21 16:43:29 -0700

    forgot to add... :(

---
+++ middleware/scraper/loaders/hack.ts
@@ -0,0 +1,29 @@
+// Does hacky things with DB that need to happen periodically
+// TODO: this should be replaced with a real scheduler soon
+
+import { ChartDao } from '../../lib/dao/charts';
+import { RunDao } from '../../lib/dao/runs';
+
+import * as scraper from '../index';
+
+export async function gen_site_total_runs(_runid: string, _options: any) {
+    try {
+        let chart = await new RunDao(scraper.storedb!).get_historical_run_count();
+        await new ChartDao(scraper.storedb!).save(chart);
+    }
+    catch(err) {
+        console.error('loader/hack: could not generate site total runs chart:', err.statusCode || err);
+        throw 'permanent';
+    }
+}
+
+export async function gen_site_volume(_runid: string, _options: any) {
+    try {
+        let chart = await new RunDao(scraper.storedb!).get_site_submission_volume();
+        await new ChartDao(scraper.storedb!).save(chart);
+    }
+    catch(err) {
+        console.error('loader/hack: could not generate site volume chart:', err.statusCode || err);
+        throw 'permanent';
+    }
+}
